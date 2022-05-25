e5c3af0705fea50125c7e569bc3c1b3d66c27759
Thendo18 <thendomakherana@gmail.com>
2022-05-25 16:28:53 +0200

    changes

--- Backend/Connection/db.js
+++ Backend/Connection/db.js
@@ -2,10 +2,10 @@ const sequelize = require('sequelize');
 
 // const sequelize= newSequelize('mysql://root:@localhost:3306/loginRegister');
 
-const sequelize= new Sequelize('loginregister', 'root', 'password', {
+const sequelizer= new sequelize('loginRegister', 'root', 'password', {
     host: '127.0.0.1',
     dialect: 'mysql',
     operatorsAliases: 0, // change this to zero
 })
 
-module.exports = sequelize;
+module.exports = sequelizer;
